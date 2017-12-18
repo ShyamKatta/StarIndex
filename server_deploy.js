c5f83663fc9c1d738e2ae2a8c94fd0b76563d95b
skatta3 <skatta3@ncsu.edu>
2017-12-18 14:16:21 -0500

    adding package file

--- server_deploy.js
+++ server_deploy.js
@@ -13,10 +13,12 @@ var fs = require('fs');
 
 // server.listen(port, hostname, () => {
 //   console.log(`Server running at http://${hostname}:${port}/`);
+const PORT = process.env.PORT || 8080;
+console.log(PORT);
 http.createServer(function (req, res) {
   var q = url.parse(req.url, true);
   var filename = "." + q.pathname;
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
+  res.end("you are accessing from "+q.hostname+" query for "+q.path);
+}).listen(PORT, '127.0.0.1');
