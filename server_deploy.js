7ae7f7f3ad85ebf0505a8ba81adcc4b26012bec7
skatta3 <skatta3@ncsu.edu>
2017-12-18 14:42:21 -0500

    updated server file

--- server_deploy.js
+++ server_deploy.js
@@ -13,12 +13,12 @@ var fs = require('fs');
 
 // server.listen(port, hostname, () => {
 //   console.log(`Server running at http://${hostname}:${port}/`);
-const PORT = process.env.PORT || 8080;
-console.log(PORT);
+//const PORT = process.env.PORT || 8080;
+//console.log(PORT);
 http.createServer(function (req, res) {
   var q = url.parse(req.url, true);
   var filename = "." + q.pathname;
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end("you are accessing from "+q.hostname+" query for "+q.path);
-}).listen(PORT, 'https://github-star-index.herokuapp.com/');
+}).listen(1337, 'https://github-star-index.herokuapp.com/');
